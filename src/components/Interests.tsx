f043fb22e81274c1d102da0f6004af8f82f2b23b
Jugin <jugin.muzhaqi7@gmail.com>
2026-07-31 01:49:49 +0100

    Clean desktop styles

--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -88,6 +88,7 @@ export function Interests() {
             type="button"
             key={interest.title}
             onClick={() => setActive(index)}
+            aria-pressed={active === index}
             variants={tabVariants}
           >
             {interest.title}
